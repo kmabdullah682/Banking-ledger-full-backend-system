c84d5fd2b39f6299b5783a43007906aa6d9719b1
Abdullah <kmabdullah682@gmail.com>
2026-07-10 01:03:32 +0600

    registration and login done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 import userModel from "../models/user.models.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, testPass } = req.body;
 
-  if (!name || !email || !password) {
+  if (!name || !email || !password || !testPass) {
     return res
       .status(400)
       .json({ message: "Please provide all required fields" });
@@ -17,8 +19,35 @@ const registerUser = async (req, res) => {
     if (existingUser) {
       return res.status(400).json({
         message: "User with the same email or name already exists",
+        success: false,
       });
     }
+
+    if (password !== testPass) {
+      return res.status(400).json({
+        message: "Passwords do not match",
+        success: false,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "5d",
+    });
+
+    res.cookie("token", token);
+
+    return res.status(201).json({
+      message: "User registered successfully",
+      success: true,
+    });
   } catch (error) {
     return res.status(500).json({
       message: "Error occurred while registering user",
@@ -26,3 +55,45 @@ const registerUser = async (req, res) => {
     });
   }
 };
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email",
+        success: false,
+      });
+    }
+
+    const isPassword = await bcrypt.compare(password, user.password);
+
+    if (!isPassword) {
+      return res.status(401).json({
+        message: "Invalid password",
+        success: false,
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "5d",
+    });
+
+    res.cookie("token", token);
+
+    return res.status(200).json({
+      message: "User logged in successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error " + error,
+      success: false,
+    });
+  }
+};
+
+export { registerUser, loginUser };
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -21,10 +21,7 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-      match: [
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-      ],
+      length: [8, "Password must be at least 8 characters long"],
       select: false,
     },
   },
