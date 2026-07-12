e0dcce9aff7644542599bce2aa8369dc424ab8d1
Abdullah <kmabdullah682@gmail.com>
2026-07-13 02:31:28 +0600

    transfer balance api done correctly

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -4,6 +4,11 @@ import mongoose from "mongoose";
 
 const DAILY_SAVINGS_LIMIT = 5;
 const DAILY_WITHDRAW_CASH_LIMIT = 10000;
+const EXCHANGE_RATES = {
+  USD: { EUR: 0.92, GBP: 0.78, USD: 1.0 },
+  EUR: { USD: 1.09, GBP: 0.85, EUR: 1.0 },
+  GBP: { USD: 1.28, EUR: 1.18, GBP: 1.0 },
+};
 
 const deposit = async (req, res) => {
   const { accountNumber, amount } = req.body;
@@ -195,4 +200,111 @@ const withdraw = async (req, res) => {
   }
 };
 
-export { deposit, withdraw };
+const transaferBalance = async (req, res) => {
+  const { sourceAccountId, destinationAccountId, amount } = req.body;
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    if (!sourceAccountId || !destinationAccountId || !amount) {
+      return res.status(400).json({
+        message:
+          "please provide your important credentials to transfer the balance",
+        success: false,
+      });
+    }
+
+    const senderAccount = await AccountModel.findOne({
+      accountNumber: sourceAccountId,
+    }).session(session);
+    const receiverAccount = await AccountModel.findOne({
+      accountNumber: destinationAccountId,
+    }).session(session);
+
+    if (!senderAccount) {
+      await session.abortTransaction();
+      return res.status(404).json({
+        message:
+          "account not found please create an account to transfer balance",
+        success: false,
+      });
+    }
+
+    if (!receiverAccount) {
+      await session.abortTransaction();
+      return res.status(404).json({
+        message:
+          "account not found please create an account to receive balance",
+        success: false,
+      });
+    }
+
+    if (senderAccount.balance < amount) {
+      await session.abortTransaction();
+      return res.status(400).json({
+        message:
+          "your account doesn't have sufficient balance to transfer please deposit balance to transfer",
+        success: false,
+      });
+    }
+
+    const senderCurrency = senderAccount.currency;
+    const receiverCurrency = receiverAccount.currency;
+
+    const conversionRate = EXCHANGE_RATES[senderCurrency][receiverCurrency];
+    const receiverAmount = Math.round(amount * conversionRate);
+
+    senderAccount.balance -= amount;
+    await senderAccount.save({ session });
+    receiverAccount.balance += receiverAmount;
+    await receiverAccount.save({ session });
+
+    const createTransactionId = (prefix = "TXN") => {
+      const timestamp = Date.now().toString(36);
+      const randomStr = Math.random().toString(36).substring(2, 8);
+      return `${prefix}-${timestamp}-${randomStr}`.toUpperCase();
+    };
+
+    const transactionId = createTransactionId();
+
+    const senderLedger = new ledgerModel({
+      transactionId,
+      accountNumber: sourceAccountId,
+      type: "debit",
+      amount,
+      description: `transfering balance of ${amount}`,
+    });
+
+    const receiverLedger = new ledgerModel({
+      transactionId,
+      accountNumber: destinationAccountId,
+      type: "credit",
+      amount: receiverAmount,
+      description: `received balance of ${receiverAmount}`,
+    });
+
+    await senderLedger.save({ session });
+    await receiverLedger.save({ session });
+
+    await session.commitTransaction();
+
+    return res.status(200).json({
+      message: "Transfer successful",
+      success: true,
+      sourceAccount: sourceAccountId,
+      destinationAccount: destinationAccountId,
+      transferredAmount: amount,
+      receivedAmount: receiverAmount,
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    return res.status(500).json({
+      message: "Internal server error " + error,
+      success: false,
+    });
+  } finally {
+    session.endSession();
+  }
+};
+
+export { deposit, withdraw, transaferBalance };
--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,10 +1,18 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middlewares.js";
-import { deposit, withdraw } from "../controllers/transaction.controller.js";
+import {
+  deposit,
+  transaferBalance,
+  withdraw,
+} from "../controllers/transaction.controller.js";
 
 const router = express.Router();
 
+//* Internal balance deposit withdraw apis
 router.post("/deposit", authMiddleware, deposit);
 router.post("/withdraw", authMiddleware, withdraw);
+
+//* Transfer balance api
+router.post("/transfer", authMiddleware, transaferBalance);
 
 export default router;
